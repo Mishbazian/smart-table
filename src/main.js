02cba1993f553b7225fa315bf1294ea556fc697d
Mikhail Ivanov <shirokopuh@gmail.com>
2026-02-12 21:57:35 +0700

    feat(components): добавить фильрацию

--- src/main.js
+++ src/main.js
@@ -8,8 +8,9 @@ import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
 // @todo: подключение
-import {initPagination} from "./components/pagination.js"
-import {initSorting} from "./components/sorting.js"
+import {initPagination} from "./components/pagination.js";
+import {initSorting} from "./components/sorting.js";
+import {initFiltering} from './components/filtering.js';
 
 
 // Исходные данные используемые в render()
@@ -38,7 +39,8 @@ function collectState() {
 function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
-    // @todo: использование 
+    // @todo: использование
+    result = applyFiltering(result, state, action);
     result = applySorting(result, state, action);
     result = applyPagination(result, state, action);
 
@@ -50,7 +52,7 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: ['header'],
+    before: ['header', 'filter'],
     after: ['pagination']
 }, render);
 
@@ -72,7 +74,13 @@ const applyPagination = initPagination(
 const applySorting = initSorting([
     sampleTable.header.elements.sortByDate,
     sampleTable.header.elements.sortByTotal
-])
+]);
+
+// - фильтрация
+const applyFiltering = initFiltering(sampleTable.filter.elements, {  // передаём элементы фильтра
+    // для элемента с именем searchBySeller устанавливаем массив продавцов
+    searchBySeller: indexes.sellers 
+});
 
 
 
