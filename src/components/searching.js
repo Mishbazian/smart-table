218a0f17ac1130c2808c0e683d4d440e18a3fe11
Mikhail Ivanov <shirokopuh@gmail.com>
2026-02-13 22:03:52 +0700

    feat(components): добавить поиск по полям таблицы
    - инициализировать поиск
    
    - настроить компаратор
    
    МЕТА ДАННЫЕ: @todo#5.1, #5.2

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,20 @@
-import {rules, createComparison} from "../lib/compare.js";
-
+import { rules, createComparison } from "../lib/compare.js";
 
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
+    // настроить компаратор
+    const defRules = ["skipEmptyTargetValues"];
+    const cusRules = [
+        rules.searchMultipleFields(
+            searchField,
+            ["date", "customer", "seller"],
+            false,
+        ),
+    ];
+
+    const compare = createComparison(defRules, cusRules);
 
     return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data;
-    }
+        //  применить компаратор
+        return data.filter((row) => compare(row, state));
+    };
 }
--- src/main.js
+++ src/main.js
@@ -7,10 +7,11 @@ import {initData} from "./data.js";
 import {processFormData, groupValueByName} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
-// @todo: подключение
+// подключение
 import {initPagination} from "./components/pagination.js";
 import {initSorting} from "./components/sorting.js";
 import {initFiltering} from './components/filtering.js';
+import { initSearching } from './components/searching.js';
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -41,12 +42,11 @@ function collectState() {
 function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
+    // использование
+    result = applySearching(result, state, action);
     result = applyFiltering(result, state, action);
     result = applySorting(result, state, action);
     result = applyPagination(result, state, action);
-
-
 
     sampleTable.render(result)
 }
@@ -54,11 +54,11 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: ['header', 'filter'],
+    before: ['search', 'header', 'filter'],
     after: ['pagination']
 }, render);
 
-// @todo: инициализация
+// инициализация
 //- пагинация
 const applyPagination = initPagination(
     sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
@@ -80,12 +80,12 @@ const applySorting = initSorting([
 
 // - фильтрация
 // передаём элементы фильтра
-// // для элемента с именем searchBySeller устанавливаем массив продавцов
+// для элемента с именем searchBySeller устанавливаем массив продавцов
 const applyFiltering = initFiltering(sampleTable.filter.elements, {
     searchBySeller: indexes.sellers,
 });
-
-
+// - поиск
+const applySearching = initSearching('search');
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
